ad1cdafbd2c588fe01c58bb796bdfab84b0fbf54
Aastha Aggarwal <54077747+Aastha26-lab@users.noreply.github.com>
2021-08-18 20:12:23 +0530

    Time: 1 ms (92.16%), Space: 37.9 MB (26.91%) - LeetHub

---
+++ decode-ways/decode-ways.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {
+        if(s==null || s.length()==0){
+            return 0;
+        }
+        int[] dp=new int[s.length()+1];
+        dp[0]=1;
+        if(s.charAt(0)!='0'){
+            dp[1]=1;
+        }else{
+            dp[1]=0;
+        }
+        for(int i=2;i<=s.length();i++){
+            int first=Integer.valueOf(s.substring(i-1,i));
+            int second=Integer.valueOf(s.substring(i-2,i));
+            if(first>=1 && first<=9){
+                dp[i]+=dp[i-1];
+            }
+            if(second>=10 && second<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[s.length()];
+    }
+}
