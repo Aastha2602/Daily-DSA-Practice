523ba4be09651ba6efabd887bfdd97d5cbfca86b
Aastha Aggarwal <54077747+Aastha26-lab@users.noreply.github.com>
2021-08-27 13:33:50 +0530

    Time: 0 ms (100.00%), Space: 37.1 MB (31.64%) - LeetHub

---
+++ longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)){
+            return -1;
+        }
+        
+        return Math.max(a.length(),b.length());
+    }
+}
