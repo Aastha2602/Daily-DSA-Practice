bba4e0c97a42884c316f1c62fc1836033fe7bc40
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-01-04 16:06:42 +0530

    Added solution - LeetHub

---
+++ Count the subarrays having product less than k - GFG/count-the-subarrays-having-product-less-than-k.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long k = Long.parseLong(stt.nextToken());
+            
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.countSubArrayProductLessThanK(a, n, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public int countSubArrayProductLessThanK(long a[], long n, long k)
+    {
+        long prod=1;
+        int start=0,end=0,count=0;
+        while(end<n){
+            prod*=a[end];
+            while(start<n && prod>=k){
+                prod=prod/a[start];
+                start++;
+            }
+            if(prod<k){
+                count+=end-start+1;
+            }
+            end++;
+        }
+        return count;
+    }
+}
