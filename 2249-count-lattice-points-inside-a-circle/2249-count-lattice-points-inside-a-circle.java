4fcc26e576f98f2cdb484e08a1552fa1a7dbbb0d
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-04-24 09:52:41 +0530

    Time: 1690 ms (25.00%), Space: 146.4 MB (25.00%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.java
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.java
@@ -1,14 +1,16 @@
 class Solution {
     public int countLatticePoints(int[][] circles) {
-    HashSet<String> res = new HashSet<String>();
-    for(int[] arr : circles){
-        int x = arr[0], y = arr[1], r = arr[2];
-        for(int i=x-r; i<=x+r; i++)
-            for(int j=y-r; j<=y+r; j++){
-                if((x-i)*(x-i) + (y-j)*(y-j) <= r*r)
-                    res.add(i+","+j);
-            }
+        HashSet<String> res = new HashSet<String>();
+        for(int[] arr : circles){
+            int x = arr[0], y = arr[1], r = arr[2];
+            for(int i=x-r; i<=x+r; i++)
+                for(int j=y-r; j<=y+r; j++){
+                    if((x-i)*(x-i) + (y-j)*(y-j) <= r*r)
+                        res.add(i+","+j);
+                // System.out.println(i+" "+j);
+                }
+            
+        }
+        return res.size();
     }
-    return res.size();
-}
 }
