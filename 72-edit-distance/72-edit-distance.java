3e9f9f8736db148c8be168bb68abae4bc705ca1f
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-05-08 09:24:24 +0530

    Time: 8 ms (57.17%), Space: 45.8 MB (6.96%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -2,27 +2,48 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
         int[][] dp = new int[n+1][m+1];
-        int ans = minDistance_memo(word1,word2,n,m,dp);
+        int ans = minDistance_tabu(word1,word2,n,m,dp);
         return ans;
     }
-    public int minDistance_memo(String s1,String s2,int n,int m,int[][] dp){
-        if(n==0 || m==0 ){
-            return dp[n][m] = n!=0 ? n : m;
-        }
+//     public int minDistance_memo(String s1,String s2,int n,int m,int[][] dp){
+//         if(n==0 || m==0 ){
+//             return dp[n][m] = n!=0 ? n : m;
+//         }
         
-        if(dp[n][m] != 0 ){
-            return dp[n][m];
-        }
+//         if(dp[n][m] != 0 ){
+//             return dp[n][m];
+//         }
         
-        int a = minDistance_memo(s1,s2,n-1,m-1,dp);
-        int b = minDistance_memo(s1,s2,n-1,m,dp);
-        int c = minDistance_memo(s1,s2,n,m-1,dp);
+//         int a = minDistance_memo(s1,s2,n-1,m-1,dp);
+//         int b = minDistance_memo(s1,s2,n-1,m,dp);
+//         int c = minDistance_memo(s1,s2,n,m-1,dp);
         
-        if(s1.charAt(n-1) == s2.charAt(m-1) ){
-            return dp[n][m] = a;
-        }else{
-            return dp[n][m] = Math.min(a,Math.min(b,c)) + 1; 
-        }
+//         if(s1.charAt(n-1) == s2.charAt(m-1) ){
+//             return dp[n][m] = a;
+//         }else{
+//             return dp[n][m] = Math.min(a,Math.min(b,c)) + 1; 
+//         }
         
+//     }
+    
+    
+    public int minDistance_tabu(String s1,String s2,int N,int M,int[][] dp){
+        
+        for(int n = 0;n<=N;n++){
+            for(int m = 0;m<=M;m++){
+                if(n==0 || m==0){
+                    dp[n][m] = n!=0 ? n:m;
+                    continue;
+                }
+                
+                if(s1.charAt(n-1) == s2.charAt(m-1)){
+                    dp[n][m] = dp[n-1][m-1];
+                }else{
+                    dp[n][m] = Math.min(dp[n-1][m-1],Math.min(dp[n-1][m],dp[n][m-1]))+1;
+                }
+            }
+        }        
+        
+     return dp[N][M];   
     }
 }
