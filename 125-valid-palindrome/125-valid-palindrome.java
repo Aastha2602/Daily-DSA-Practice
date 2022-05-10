80bea838b672ac80d7d55195cf957f27be469d29
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-05-10 10:42:05 +0530

    Time: 3 ms (94.26%), Space: 42.3 MB (91.61%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,7 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String actual = s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
-        String rev = new StringBuffer(actual).reverse().toString();
-        return actual.equals(rev);
+        char[] arr = s.toCharArray();
+        for(int i=0,j=arr.length-1;i<j; ){
+            if(!Character.isLetterOrDigit(arr[i])){
+                i++;
+            }else if(!Character.isLetterOrDigit(arr[j])){
+                j--;
+            }else if(Character.toLowerCase(arr[i++]) != Character.toLowerCase(arr[j--])){
+                return false;
+            }
+        }
+        return true;
     }
 }
