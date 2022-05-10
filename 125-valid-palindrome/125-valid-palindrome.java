af62c9de03a7ee2086c9c789d27f20dbc1ccb820
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-05-10 10:36:34 +0530

    Time: 710 ms (21.58%), Space: 47 MB (22.67%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
+}
