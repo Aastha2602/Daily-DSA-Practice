51faa42b6090c30f4499d9bab9a406efde4ee7e5
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-05-28 11:55:06 +0530

    Time: 1 ms (65.97%), Space: 50.6 MB (65.36%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0;i<nums.length;i++){
+            xor = xor ^ (i+1) ^ nums[i]; 
+        }
+        return xor;
+    }
+}
