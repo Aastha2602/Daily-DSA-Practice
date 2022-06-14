c7dfd0745c85ebbf7b65fc378693f8c241468474
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-06-14 08:58:05 +0530

    Time: 1 ms (99.88%), Space: 45 MB (7.75%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    private Integer[][] memo;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        memo = new Integer[n][n];
+        return dfs(0,0,triangle);
+    }
+    
+    private int dfs(int level, int i, List<List<Integer>> triangle){
+        if(memo[level][i] != null ){
+            return memo[level][i];
+        }
+        
+        int path = triangle.get(level).get(i);
+        if(level<triangle.size() -1 ){
+            path += Math.min(dfs(level+1,i,triangle) , dfs(level+1,i+1,triangle));
+        }
+        return memo[level][i] = path;
+    }
+    
+}
