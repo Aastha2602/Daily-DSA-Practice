4db6092626edf4a75b5c0276dd549ddf2c59f508
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-07-03 18:30:52 +0530

    Time: 130 ms (14.23%), Space: 135 MB (22.02%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,4 +1,5 @@
 // head -- mru element -- other elements -- lru element -- tail
+
 // we insert everything to the right after head 
 // lru is the element right before the tail 
 
@@ -35,6 +36,7 @@ class LRUCache {
         }
     }
     
+    // if the cache is full, lru element needs to be removed that is "tail.prev" will be removed
     public void put(int key, int value) {
         if( map.containsKey(key) ){
             remove(map.get(key));
@@ -45,12 +47,14 @@ class LRUCache {
         insert(new Node(key,value));
     }
     
+    // when the current pointer is on the node to be deleted, just cut and join the required edges 
     private void remove(Node node){
         map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
     
+    // inserting to the just right of the head
     private void insert(Node node){
         map.put(node.key, node);
         node.next = head.next;
