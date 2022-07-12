fdde2c38ef1636310856856ee230d784422e5544
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-07-12 20:25:29 +0530

    Added solution - LeetHub

---
+++ Pythagorean Triplet - GFG/pythagorean-triplet.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().checkTriplet(arr, n);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        // code here
+        if(n<3){
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        Arrays.sort(arr);
+        for(int val:arr){
+            set.add(val*val);
+        }
+        
+        for(int i=n-1;i>=2;i--){
+            for(int j=i-1;j>=1;j--){
+                if(set.contains(arr[i]*arr[i]-arr[j]*arr[j])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
