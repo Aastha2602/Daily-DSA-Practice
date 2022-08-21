362773e2920911a159f784e38b730cd6c714683a
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-08-21 18:02:59 +0530

    Time: 24 ms (53.76%), Space: 51.4 MB (54.06%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.java
@@ -1,16 +1,83 @@
+class Node {
+    Node[] links = new Node[26];
+    boolean flag = false;
+    
+    public Node() {
+        
+    }
+    
+    boolean containsKey(char ch){
+        return ( links[ch-'a'] != null );
+    }
+    
+    Node get(char ch){
+        return links[ch-'a'];
+    }
+    
+    void put(char ch, Node node){
+        links[ch - 'a'] = node;
+    }
+    
+    void setEnd(){
+        flag = true;
+    }
+    
+    boolean isEnd(){
+        return flag;
+    }
+}
+
+class Trie {
+    public Node root;
+    
+    Trie() {
+        root = new Node();
+    }
+    
+    public void insert(String word){
+        Node temp = root;
+        for(int i=0;i<word.length();i++){
+            if(!temp.containsKey(word.charAt(i))){
+                temp.put(word.charAt(i), new Node());
+            }
+            temp = temp.get(word.charAt(i));
+        }
+        temp.setEnd();
+    }
+    
+    public boolean checkIfPrefixExists(String word){
+        Node temp = root;
+        boolean flag = true;
+        for(int i=0;i<word.length();i++){
+            if(temp.containsKey(word.charAt(i))){
+                temp = temp.get(word.charAt(i));
+                flag = flag & temp.isEnd();
+            }else return false;
+        }
+        
+        return flag;
+    }
+}
+
 class Solution {
     public String longestWord(String[] words) {
-        Arrays.sort(words);
-        HashSet<String> set = new HashSet<>();
-        String res = "";
-        for(String w:words ){
-            int len = w.length();
-            if( len == 1 || set.contains(w.substring(0, len-1))){
-                res = len > res.length() ? w : res ;
-                set.add(w);
+        Trie obj = new Trie();
+        for(int i=0;i<words.length;i++){
+            obj.insert(words[i]);
+        }
+        
+        String longest = "";
+        for(int i=0;i<words.length;i++){
+            if(obj.checkIfPrefixExists(words[i])){
+                if( words[i].length() > longest.length() ){
+                    longest = words[i];
+                }else if( words[i].length() == longest.length() && words[i].compareTo(longest) < 0 ){
+                    longest = words[i];
+                }
             }
         }
         
-        return res;
+        if(longest == "" ) return "";
+        else return longest;
     }
 }
