3b23bc3654dcede4a52f854fc15081aa21f2058d
Aastha Aggarwal <54077747+Aastha2602@users.noreply.github.com>
2022-09-17 15:32:54 +0530

    Time: 4 ms (22.60%), Space: 41.7 MB (71.00%) - LeetHub

---
+++ 274-h-index/274-h-index.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        return hIndexII(citations);
+    }
+    
+    public int hIndexII(int[] citations){
+        int[] bucket = new int[citations.length+1];
+        
+        for(int i=0;i<citations.length;i++){
+            if(citations[i] > citations.length){
+                bucket[citations.length]++;
+            }else{
+                bucket[citations[i]]++;
+            }
+        }
+        
+        int count = 0;
+        for(int i=bucket.length-1;i>=0;i--){
+            count += bucket[i];
+            if(count >= i){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
